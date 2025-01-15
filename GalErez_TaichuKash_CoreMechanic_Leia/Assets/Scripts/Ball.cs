4d9ad4653dab3845b206c6c580e26c7edfc6e54f
Daimnon <49341329+Daimnon@users.noreply.github.com>
2025-01-15 16:54:46 +0200

    Reached desired physics

--- GalErez_TaichuKash_CoreMechanic_Leia/Assets/Scripts/Ball.cs
+++ GalErez_TaichuKash_CoreMechanic_Leia/Assets/Scripts/Ball.cs
@@ -6,4 +6,27 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] BallType _ballType;
     public BallType @BallType => _ballType;
+
+    [SerializeField] private Rigidbody2D _rb2D;
+    [SerializeField] private float _upwardPushForce = 1.0f;
+
+    /*private void FixedUpdate()
+    {
+        _rb2D.AddForce(_upwardPushForce * Vector2.up, ForceMode2D.Force);
+    }*/
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            Rigidbody2D otherRigidbody = collision.rigidbody;
+
+            if (otherRigidbody != null)
+            {
+                // Calculate the downward force to apply based on weightTransferForce
+                Vector2 upwardForce = Vector2.up * _upwardPushForce;
+                otherRigidbody.AddForce(upwardForce, ForceMode2D.Force);
+            }
+        }
+    }
 }
