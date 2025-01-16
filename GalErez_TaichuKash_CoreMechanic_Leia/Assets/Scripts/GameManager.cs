41aa26b933cc63f4218e2a969bd311e4ac489cfc
Daimnon <49341329+Daimnon@users.noreply.github.com>
2025-01-16 19:05:09 +0200

    Hierarchy and time

--- GalErez_TaichuKash_CoreMechanic_Leia/Assets/Scripts/GameManager.cs
+++ GalErez_TaichuKash_CoreMechanic_Leia/Assets/Scripts/GameManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Timer")]
+    [SerializeField] private float _levelTime = 300.0f;
+    [SerializeField] private TextMeshProUGUI _timerText;
+
+    private float _timeRemaining;
+    private void Awake()
     {
-        
+        _timeRemaining = _levelTime;
+    }
+    private void Update()
+    {
+        if (_timeRemaining > 0)
+        {
+            DecreaseTime();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTimerText(float timeSinceStartup)
     {
-        
+        int minutes = Mathf.FloorToInt(timeSinceStartup / 60) % 60;
+        int seconds = Mathf.FloorToInt(timeSinceStartup) % 60;
+
+        _timerText.text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+    }
+    private void DecreaseTime()
+    {
+        _timeRemaining -= Time.deltaTime;
+        UpdateTimerText(_timeRemaining);
+
+        if (_timeRemaining <= 0)
+        {
+            GameOver();
+        }
+    }
+    private void GameOver()
+    {
+        // do pop up
     }
 }
