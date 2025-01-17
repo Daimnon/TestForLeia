4b930b748255d9e5962e3da7b57f0a70c2f75281
Daimnon <49341329+Daimnon@users.noreply.github.com>
2025-01-17 21:31:53 +0200

    Implemented Alpha - Beta pruning

--- GalErez_TicTacToe_Minimax_ABPruning_Leia/Assets/GameManager.cs
+++ GalErez_TicTacToe_Minimax_ABPruning_Leia/Assets/GameManager.cs
@@ -13,10 +13,14 @@ public enum Difficulity
 
 public class GameManager : MonoBehaviour
 {
+    private const int MIN_VALUE = -10000; // just a randomly extreme low value
+    private const int MAX_VALUE = 10000; // just a randomly extreme high value
+
     private int[] _board; // generated on start
+    private int _turn = 1; // turn = 1 implies player 1 turn, same for player 2 aka the bot
+    private string[] _shapes = new string[] { string.Empty, string.Empty , string.Empty }; // first shape should be always empty, 'x' and 'o' will be randomly assign afterwards.
 
     [Header("Configuration")]
-    [SerializeField] private int _turn = 1; // turn = 1 implies player 1 turn, same for player 2 aka the bot
     [SerializeField] private float _botMoveDelay = 0.4f;
     [SerializeField] private Difficulity _botLevel = Difficulity.Hard;
     [SerializeField] private int[] _botForeSightDepth = new int[4] { 1, 2, 4, 9 };
@@ -24,7 +28,6 @@ public class GameManager : MonoBehaviour
 
     [Header("Tiles")]
     [SerializeField] private TextMeshProUGUI[] _tileTexts;
-    [SerializeField] private string[] _shapes; // first shape should be empty
 
     [Header("Game State UI")]
     [SerializeField] private TextMeshProUGUI _turnTextTMP;
@@ -35,8 +38,17 @@ public class GameManager : MonoBehaviour
         ResetGame();
     }
 
+    private void RandomizeShapes()
+    {
+        List<string> possibleShapes = new List<string> { "X", "O" };
+
+        _shapes[1] = possibleShapes[Random.Range(0, possibleShapes.Count)];
+        _shapes[2] = _shapes[1] == "X" ? "O" : "X";
+    }
     private void ResetGame()
     {
+        RandomizeShapes();
+
         _board = new int[9];
         _turn = 1;
 
@@ -70,7 +82,7 @@ public class GameManager : MonoBehaviour
         if (_turn == 2)
         {
             int depth = _botForeSightDepth[(int)_botLevel];
-            Minimax(_board, depth, true, depth);
+            Minimax(_board, depth, true, depth, MIN_VALUE, MAX_VALUE);
         }
     }
 
@@ -102,17 +114,19 @@ public class GameManager : MonoBehaviour
         newBoard[move] = turn;
         return newBoard;
     }
-    private int MaxPlayer(int[] currentBoard, int depth, int initialDepth)
+    private int MaxPlayer(int[] currentBoard, int depth, int initialDepth, int alpha, int beta)
     {
-        int maxEvaluation = -10000; // just a randomly extreme low value
-        List<int> possibleMoves = GetPossibleMoves(currentBoard);
+        int maxEvaluation = MIN_VALUE;
+        List<int> possibleMoves = GetPossibleMoves(currentBoard); // get available moves
         List<int> bestMoves = new();
 
         foreach (int move in possibleMoves)
         {
+            // creating a temporary board in order to evaluate minimax
             int[] tempBoard = CreateTempBoard(currentBoard, move, _turn);
-            int evaluation = Minimax(tempBoard, depth - 1, false, initialDepth);
+            int evaluation = Minimax(tempBoard, depth - 1, false, initialDepth, alpha, beta);
 
+            // storing best moves
             if (initialDepth == depth)
             {
                 if (evaluation > maxEvaluation)
@@ -126,8 +140,15 @@ public class GameManager : MonoBehaviour
                 }
             }
             maxEvaluation = Mathf.Max(maxEvaluation, evaluation);
+
+            if (_useAlphaBetaPruning)
+            {
+                alpha = Mathf.Max(alpha, maxEvaluation);
+                if (beta <= alpha) break; // beta cutoff - if the beta score (best of minimizer) is less or equal than the alpha, it means there is no need to try more options
+            }
         }
 
+        // If at the root level, choose a random best move
         if (initialDepth == depth && bestMoves.Count > 0)
         {
             int theBestMove = bestMoves[Random.Range(0, bestMoves.Count)];
@@ -136,29 +157,36 @@ public class GameManager : MonoBehaviour
 
         return maxEvaluation;
     }
-    private int MinPlayer(int[] currentBoard, int depth, int initialDepth)
+    private int MinPlayer(int[] currentBoard, int depth, int initialDepth, int alpha, int beta)
     {
-        int minEvaluation = 10000; // just a randomly extreme high value
-        List<int> possibleMoves = GetPossibleMoves(currentBoard);
+        int minEvaluation = MAX_VALUE;
+        List<int> possibleMoves = GetPossibleMoves(currentBoard); // get available moves
 
         foreach (int move in possibleMoves)
         {
             int[] tempBoard = CreateTempBoard(currentBoard, move, _turn == 1 ? 2 : 1);
-            int evaluation = Minimax(tempBoard, depth - 1, true, initialDepth);
+            int evaluation = Minimax(tempBoard, depth - 1, true, initialDepth, alpha, beta);
+
             minEvaluation = Mathf.Min(minEvaluation, evaluation);
+
+            if (_useAlphaBetaPruning)
+            {
+                beta = Mathf.Min(beta, minEvaluation);
+                if (alpha >= beta) break; // alpha cutoff - if the alpha score (best of maximizer) is greater than or equal to the beta, it means there is no need to try more options
+            }
         }
 
         return minEvaluation;
     }
-    private int Minimax(int[] currentBoard, int depth, bool isMaxSide, int initialDepth)
+    private int Minimax(int[] currentBoard, int depth, bool isMaxSide, int initialDepth, int alpha, int beta)
     {
         int stateEvaluation = EvaluateState(currentBoard, depth); // could be terminal state or depth state
 
-        if (stateEvaluation != int.MinValue) 
+        if (stateEvaluation != int.MinValue)
             return stateEvaluation;
 
-        if (isMaxSide) return MaxPlayer(currentBoard, depth, initialDepth);
-        else return MinPlayer(currentBoard, depth, initialDepth);
+        if (isMaxSide) return MaxPlayer(currentBoard, depth, initialDepth, alpha, beta);
+        else return MinPlayer(currentBoard, depth, initialDepth, alpha, beta);
     }
     #endregion
 
